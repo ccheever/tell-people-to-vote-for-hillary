5cbd98a0f7bc0bb4eb54ae2471a55d9a9891e6e0
Charlie Cheever <ccheever@gmail.com>
2016-09-24 01:15:02 -0700

    Don't ask for permissions for push notifs

--- api/registerForPushNotificationsAsync.js
+++ api/registerForPushNotificationsAsync.js
@@ -12,6 +12,7 @@ const PUSH_ENDPOINT = 'https://exponent-push-server.herokuapp.com/tokens';
 export default async function registerForPushNotificationsAsync() {
   // Android remote notification permissions are granted during the app
   // install, so this will only ask on iOS
+  /*
   let { status } = await Permissions.askAsync(Permissions.REMOTE_NOTIFICATIONS);
 
   // Stop here if the user did not grant permissions
@@ -19,9 +20,12 @@ export default async function registerForPushNotificationsAsync() {
     return;
   }
 
+
   // Get the token that uniquely identifies this device
   let token = await Notifications.getExponentPushTokenAsync();
 
+  */
+  
   // POST the token to our backend so we can use it to send pushes from there
   /*
   return fetch(PUSH_ENDPOINT, {
