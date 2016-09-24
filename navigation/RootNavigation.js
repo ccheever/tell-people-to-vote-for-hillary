05f97c7d1a995fa4948ed9de03dfee665f4c4c14
James Ide <ide@jameside.com>
2016-09-24 03:02:04 -0700

    Remove unused push notification code

--- navigation/RootNavigation.js
+++ navigation/RootNavigation.js
@@ -2,7 +2,6 @@ import React, {
   PropTypes
 } from 'react';
 import {
-  DeviceEventEmitter,
   Image,
   Linking,
   StyleSheet,
@@ -21,7 +20,6 @@ import {
 import Alerts from '../constants/Alerts';
 import Colors from '../constants/Colors';
 import Router from '../navigation/Router';
-import registerForPushNotificationsAsync from '../api/registerForPushNotificationsAsync';
 
 import {
   StateIcons,
@@ -30,14 +28,6 @@ import {
 let st = 'OH';
 
 export default class RootNavigation extends React.Component {
-  componentDidMount() {
-    this._notificationSubscription = this._registerForPushNotifications();
-  }
-
-  componentWillUnmount() {
-    this._notificationSubscription && this._notificationSubscription.remove();
-  }
-
   render() {
     return (
       <TabNavigation
@@ -82,29 +72,6 @@ export default class RootNavigation extends React.Component {
         source={StateIcons[name]}
         style={{height: 40, width: 40,}}
       />
-    );
-  }
-
-  _registerForPushNotifications() {
-    const { notification } = this.props;
-
-    // Send our push token over to our backend so we can receive notifications
-    // You can comment the following line out if you want to stop receiving
-    // a notification every time you open the app. Check out the source
-    // for this function in api/registerForPushNotificationsAsync.js
-    registerForPushNotificationsAsync();
-
-    // If we started the app from a push notification, handle it right away
-    notification && this._handleNotification(notification);
-
-    // Handle notifications that come in while the app is open
-    return DeviceEventEmitter.addListener('Exponent.notification', this._handleNotification);
-  }
-
-  _handleNotification = ({origin, data}) => {
-    this.props.navigator.showLocalAlert(
-      `Push notification ${origin} with data: ${data}`,
-      Alerts.notice
     );
   }
 }
